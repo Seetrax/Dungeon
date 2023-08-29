9a8ab651ab07f2ac93cabab00d0fb72750e8c0cb
Vijay <vijaynaruto8@gmail.com>
2023-08-30 01:56:52 +0530

    Save local changes

--- Gametest/Test.py
+++ Gametest/Test.py
@@ -1,16 +1,23 @@
 import pygame
 import sys
+import os
 
+DIRECTION=0
 
 pygame.init()
+WIDTH, HEIGHT = 400,400
+COL, ROW = 4,4
+GRID_SIZE = WIDTH//COL
 
-GRID_SIZE = 40
-GRID_WIDTH, GRID_HEIGHT = 19,19
-WIDTH, HEIGHT = GRID_WIDTH*GRID_SIZE , GRID_HEIGHT*GRID_SIZE
+
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
+PLAYER_IMG=pygame.transform.scale(pygame.image.load(os.path.join('Agents','Player.jpg')),(GRID_SIZE,GRID_SIZE))
+WUMPUS_IMG=pygame.transform.scale(pygame.image.load(os.path.join('Agents','Wumpus.jpg')),(GRID_SIZE,GRID_SIZE))
+PIT_IMG=pygame.transform.scale(pygame.image.load(os.path.join('Agents','pit.png')),(GRID_SIZE,GRID_SIZE))
+GOLD_IMG=pygame.transform.scale(pygame.image.load(os.path.join('Agents','Gold.png')),(GRID_SIZE,GRID_SIZE))
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Grid Game")
@@ -24,7 +31,17 @@ def draw_grid():
         pygame.draw.line(screen, WHITE, (0, y), (WIDTH, y))
 
 def draw_player(x, y):
-    pygame.draw.rect(screen, GREEN, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+    if DIRECTION==0:##RIGHT
+        
+        screen.blit(pygame.transform.flip(PLAYER_IMG,True,False),(player_x* GRID_SIZE,player_y* GRID_SIZE))
+    elif DIRECTION==1:##LEFT
+
+        screen.blit(PLAYER_IMG,(player_x* GRID_SIZE,player_y* GRID_SIZE))
+    elif DIRECTION==2:##UP
+        screen.blit(pygame.transform.rotate(PLAYER_IMG,270),(player_x* GRID_SIZE,player_y* GRID_SIZE))
+    elif DIRECTION==3:##DOWN
+        screen.blit(pygame.transform.rotate(PLAYER_IMG,90),(player_x* GRID_SIZE,player_y* GRID_SIZE))
+    #pygame.draw.rect(screen, GREEN, (x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE))
 
 running = True
 while running:
@@ -36,12 +53,16 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP and player_y > 0:
                 player_y -= 1
-            elif event.key == pygame.K_DOWN and player_y < GRID_HEIGHT - 1:
+                DIRECTION=2
+            elif event.key == pygame.K_DOWN and player_y < ROW - 1:
                 player_y += 1
+                DIRECTION=3
             elif event.key == pygame.K_LEFT and player_x > 0:
                 player_x -= 1
-            elif event.key == pygame.K_RIGHT and player_x < GRID_WIDTH - 1:
+                DIRECTION=1
+            elif event.key == pygame.K_RIGHT and player_x < COL - 1:
                 player_x += 1
+                DIRECTION=0
 
 
     screen.fill((0, 0,0))
